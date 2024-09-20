616931f6dfa69b27eb101e78274534c24e78a53a
pguin-sudo <mailfordevelopmentandstuff@gmail.com>
2024-09-21 00:39:52 +0500

    Static circles

--- src/game.rs
+++ src/game.rs
@@ -3,7 +3,7 @@ use piston_window::*;
 use rand::Rng;
 
 use crate::physics::{ Circle, Engine };
-use crate::settings::{ NUM_CIRCLES, SCREEN_HEIGHT, SCREEN_WIDTH };
+use crate::settings::{ NUM_CIRCLES, NUM_STATIC_CIRCLES, SCREEN_HEIGHT, SCREEN_WIDTH };
 
 pub struct Random;
 
@@ -12,19 +12,23 @@ impl Random {
         Random
     }
 
-    pub fn get_random_circle(&self) -> Circle {
-        let mut rng = rand::thread_rng();
+    pub fn get_random_circle(&self, is_static: bool) -> Circle {
+        let mut rng: rand::prelude::ThreadRng = rand::thread_rng();
+
+        let mass: f32 = rng.gen::<f32>();
+        let amortization = rng.gen::<f32>() / 4_f32 + 0.75_f32;
+
         Circle::new(
             Point2::new(
                 rng.gen_range(0..SCREEN_WIDTH) as f32,
                 rng.gen_range(0..SCREEN_HEIGHT) as f32
             ),
             Vector2::new(0 as f32, 0 as f32),
-            0.5_f32,
-            rng.gen::<f32>() * 10_f32,
-            10_f32,
-            [rng.gen(), rng.gen(), rng.gen(), 1.0],
-            false
+            amortization,
+            mass * 10_f32,
+            rng.gen::<f32>() * 10_f32 + 10_f32,
+            [mass, 0.9, amortization, 1.0],
+            is_static
         )
     }
 }
@@ -45,7 +49,11 @@ impl Game {
     pub fn init(&mut self) {
         let random = Random::get_instance();
         for _ in 0..NUM_CIRCLES {
-            self.physics_engine.add_circle(random.get_random_circle());
+            self.physics_engine.add_circle(random.get_random_circle(false));
+        }
+
+        for _ in 0..NUM_STATIC_CIRCLES {
+            self.physics_engine.add_circle(random.get_random_circle(true));
         }
     }
 
